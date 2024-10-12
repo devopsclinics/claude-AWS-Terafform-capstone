c549f6819ce30a8974551a5d5af86c11baa1f22d
lala <cestbellespas@gmail.com>
2024-10-12 14:29:22 +0000

    added cloudfront to main.tf

--- main.tf
+++ main.tf
@@ -152,6 +152,117 @@ resource "aws_route_table_association" "db" {
   route_table_id = aws_route_table.db.id
 }
 
+resource "aws_s3_bucket" "mys3bck077lala" {
+  bucket = "my-tf-test-bucket-77lala"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "example" {
+  comment = "Example Origin Access Identity"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "clfd" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Example CloudFront Distribution"
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = "my-tf-test-bucket-77lala.s3.amazonaws.com"  
+    origin_id   = "ExampleOrigin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.example.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "ExampleOrigin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# S3 bucket for CloudTrail logs
+resource "aws_s3_bucket" "cloudtrail_bucket-007" {
+  bucket        = "example-cloudtrail-logs-bucket-007"
+  force_destroy = true
+}
+
+
+# CloudTrail
+resource "aws_cloudtrail" "example" {
+  name                          = "example-cloudtrail"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket-007.id
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_logging                = true
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
+  bucket = aws_s3_bucket.cloudtrail_bucket-007.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.cloudtrail_bucket-007.arn
+      },
+      {
+        Sid    = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.cloudtrail_bucket-007.arn}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
 
 
 data "aws_availability_zones" "available" {
