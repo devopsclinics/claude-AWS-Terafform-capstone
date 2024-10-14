23ac7a6834227b9ec4fdb060a17546a0ece2f66e
Babatunde Bankole <Ope_bankole99@yahoo.com>
2024-10-14 10:38:26 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,5 +2,19 @@
 variable "count" {
 description = " Test count"
 type        = number
-default = 2
+
+}
+
+# count variable definition
+variable "pub_subnet_count" {
+description = " Test count"
+type        = number
+
+}
+
+# count variable definition
+variable "private_subnet_count" {
+description = " Test count"
+type        = number
+
 }
